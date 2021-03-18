cb3cea1537c4fe6978d1ea4fa9726a3133f29fe7
Arash Yousefzadeh <yousefa@colorado.edu>
2021-03-18 12:51:14 -0600

    Changed flag detection back to & INT

--- project3/detector.py
+++ project3/detector.py
@@ -10,12 +10,12 @@ countSyn = Counter()
 countACK = Counter()
 
 for packet in PcapReader(infile):
-    # if TCP in packet and packet[TCP].flags & 2:  # TCP SYN packet
-    if TCP in packet and packet[TCP].flags == 'S':  # TCP SYN packet
+    if TCP in packet and packet[TCP].flags & 2 and not packet[TCP].flags & 16:  # TCP SYN packet
+    # if TCP in packet and packet[TCP].flags == 'S':  # TCP SYN packet
         src = packet.sprintf('{IP:%IP.src%}{IPv6:%IPv6.src%}')
         countSyn[src] += 1
-    # if TCP in packet and packet[TCP].flags & 16: # TCP SYN+ACK packet
-    if TCP in packet and packet[TCP].flags == 'SA': # TCP SYN+ACK packet
+    if TCP in packet and packet[TCP].flags & 2 and packet[TCP].flags & 16: # TCP SYN+ACK packet
+    # if TCP in packet and packet[TCP].flags == 'SA': # TCP SYN+ACK packet
         src = packet.sprintf('{IP:%IP.src%}{IPv6:%IPv6.src%}')
         countACK[src] += 1
 
